d5f8bf2e97c1ba2d09e0484be27e53ee46282ed4
qiuhong <qiuhong@zocono.com>
2023-12-12 17:30:13 +0800

    添加项目文件。

---
+++ RenameVSProject/Program.cs
@@ -0,0 +1,89 @@
+﻿using RenameVSProject;
+using System.Diagnostics;
+using System.Text.RegularExpressions;
+
+var path = ReadLine("请输入项目所在路径：", Directory.Exists);
+var srcStr = ReadLine("请输入要替换的字符串：", null);
+var tarStr = ReadLine("请输入新字符串：", null);
+
+var sw = new Stopwatch();
+sw.Start();
+
+var srcRegex = new Regex(srcStr, RegexOptions.Compiled | RegexOptions.IgnoreCase);
+ScanDirectory(path, srcRegex, tarStr);
+
+sw.Stop();
+Console.WriteLine(sw.Elapsed);
+
+void ScanDirectory(string path, in Regex src, in string target)
+{
+    var files = Directory.GetFiles(path).FileFilter();
+    foreach (var filePath in files)
+    {
+        var fileName = Path.GetFileName(filePath);
+        var basePath = Path.GetDirectoryName(filePath)!;
+
+        ReplaceFileContent(filePath, src, target);
+
+        if (!src.IsMatch(fileName)) continue;
+
+        var newFileName = src.Replace(fileName, target);
+        var newFilePath = Path.Combine(basePath, newFileName);
+        File.Move(filePath, newFilePath);
+    }
+
+    var dirs = Directory.GetDirectories(path).IgnoreDirFilter();
+    foreach (var dirPath in dirs)
+    {
+        var dirName = Path.GetFileName(dirPath);
+
+        ScanDirectory(dirPath, src, target);
+    }
+
+    var rootName = Path.GetFileName(path);
+    var rootBasePath = Path.GetDirectoryName(path)!;
+
+    if (src.IsMatch(rootName))
+    {
+        var newDirName = src.Replace(rootName, target);
+        var newDirPath = Path.Combine(rootBasePath, newDirName);
+        Directory.Move(path, newDirPath);
+    }
+}
+
+void ReplaceFileContent(string filePath, in Regex src, in string target)
+{
+    var content = string.Empty;
+    using (var reader = new StreamReader(filePath))
+    {
+        content = reader.ReadToEnd();
+        if (content == null) return;
+    }
+
+    var newContent = src.Replace(content, target);
+    using var writer = new StreamWriter(filePath);
+
+    writer.Write(newContent);
+}
+
+string ReadLine(string msg, Predicate<string>? pred)
+{
+    var line = string.Empty;
+    var isFirst = true;
+
+    while (string.IsNullOrEmpty(line) || !(pred?.Invoke(line) ?? true))
+    {
+        if (!isFirst)
+        {
+            Console.Write("格式错误，");
+        }
+        else
+        {
+            isFirst = false;
+        }
+
+        Console.WriteLine(msg);
+        line = Console.ReadLine()?.Trim();
+    }
+    return line;
+}
---
+++ RenameVSProject/StringsExtensions.cs
@@ -0,0 +1,132 @@
+﻿using System.Text;
+using System.Text.RegularExpressions;
+
+namespace RenameVSProject
+{
+    public static class StringsExtensions
+    {
+        private static Regex[]? s_fileRegexes;
+
+        private static Regex[] FileRegexes
+        {
+            get
+            {
+                if (s_fileRegexes == null)
+                {
+                    string[] filters = [
+                        "*.cs",
+                        "*.xaml",
+                        "*.json",
+                        "*.sln",
+                        "*.csproj",
+                        "*.xml"];
+                    s_fileRegexes = CreateRegexes(filters);
+                }
+                return s_fileRegexes;
+            }
+        }
+
+        private static Regex[]? s_ignoreDirRegexes;
+
+        private static Regex[] IgnoreDirRegexes
+        {
+            get
+            {
+                if (s_ignoreDirRegexes == null)
+                {
+                    string[] filters = [
+                        ".*",
+                        "build",
+                        "docs",
+                        "lib",
+                        "obj"];
+                    s_ignoreDirRegexes = CreateRegexes(filters);
+                }
+                return s_ignoreDirRegexes;
+            }
+        }
+
+        private static Regex[] CreateRegexes(in string[] filters)
+        {
+            var length = filters.Length;
+            var results = new Regex[length];
+
+            for (var i = 0; i < length; i++)
+            {
+                var regular = CreateRegularString(filters[i]);
+
+                results[i] = new Regex(regular, RegexOptions.Compiled);
+            }
+
+            return results;
+        }
+
+        private static string CreateRegularString(string filter)
+        {
+            var sb = new StringBuilder(filter.Length << 1);
+
+            sb.Append('^');
+
+            for (var i = 0; i < filter.Length; i++)
+            {
+                var ch = filter[i];
+                if (ch == '.')
+                {
+                    sb.Append("\\.");
+                }
+                else if (ch == '*')
+                {
+                    sb.Append(".+");
+                }
+                else
+                {
+                    sb.Append(ch);
+                }
+            }
+            sb.Append('$');
+            return sb.ToString();
+        }
+
+        public static string[] FileFilter(this string[] files)
+        {
+            var results = new List<string>(files.Length);
+
+            foreach (var filePath in files)
+            {
+                var fileName = Path.GetFileName(filePath);
+                foreach (var regex in FileRegexes)
+                {
+                    if (regex.IsMatch(fileName))
+                    {
+                        results.Add(filePath);
+                    }
+                }
+            }
+            return [.. results];
+        }
+
+        public static string[] IgnoreDirFilter(this string[] dirs)
+        {
+            var results = new List<string>(dirs.Length);
+
+            foreach (var dirPath in dirs)
+            {
+                var dirName = Path.GetFileName(dirPath);
+                var isIgnore = false;
+                foreach (var regex in IgnoreDirRegexes)
+                {
+                    if (regex.IsMatch(dirName))
+                    {
+                        isIgnore = true;
+                        break;
+                    }
+                }
+                if (!isIgnore)
+                {
+                    results.Add(dirPath);
+                }
+            }
+            return [.. results];
+        }
+    }
+}
